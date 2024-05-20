f095aa10b46b1cd07ce776f26555844cf65d0438
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-05-20 23:53:29 +0530

    First File

---
+++ no_01_AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class no_01_AddTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("The Sum Of Given Number is= ");
+
+        int num1=2;
+        int num2=4;
+
+        int sum= num1+num2;
+        System.out.println(sum);
+    }
+}
