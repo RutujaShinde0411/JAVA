689fd592bed602b0a080f00df8691dad31edd60b
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-05-23 23:09:51 +0530

    First File

--- no_01_AddTwoNumbers.java
+++ no_01_AddTwoNumbers.java
@@ -6,8 +6,9 @@ public class no_01_AddTwoNumbers {
 //        addition of two number
         int num1=2;
         int num2=4;
+        int num3=2;
 
-        int sum= num1+num2;
+        int sum= num1+num2+num3;
         System.out.println(sum);
     }
 }
