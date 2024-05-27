8c213eb05f64a45f3243f5eddb8afc1d3c5994c7
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-05-27 22:27:27 +0530

    Java Program

---
+++ no_05_GreetUser.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class no_05_GreetUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your Name: ");
+        String name = sc.next();
+
+        System.out.println("Hello " + name + " Have A Good Day!!!");
+        }
+    }
+
