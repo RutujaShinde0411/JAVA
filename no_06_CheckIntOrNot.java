0957226c5aa15a90905b77c4d737addae325081a
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-05-27 22:34:05 +0530

    Java Program

---
+++ no_06_CheckIntOrNot.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class no_06_CheckIntOrNot {
+    public static void main(String[] args) {
+
+        System.out.println("Enter A Integer Number: ");
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
