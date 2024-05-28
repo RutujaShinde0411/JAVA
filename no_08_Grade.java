8dcb4d324706f6f23b3b693f92d76beab49d337f
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-05-28 23:51:28 +0530

    Java Program

---
+++ no_08_Grade.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class no_08_Grade {
+    public static void main(String[] args) {
+        char grade='B';
+        grade=(char) (grade+8);
+        System.out.println(grade);
+
+        grade=(char) (grade-8);
+        System.out.println(grade);
+    }
+}
