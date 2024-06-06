87b7189ee22a1842c761c179267171f03f82792d
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-06-06 21:54:37 +0530

    Java Program

---
+++ no_12_Fill_Letter_Template.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class no_12_Fill_Letter_Template {
+    public static void main(String[] args) {
+        String letter="Dear name, Thanks a lot";
+        letter=letter.replace("name","Rutuja");
+        System.out.println(letter);
+    }
+}
