0f936482c7bb50d738e7b308baa8fb19789e60b7
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-06-06 21:51:36 +0530

    Java Program

---
+++ no_11_ConvertStringToLowercase.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class no_11_ConvertStringToLowercase {
+    public static void main(String[] args) {
+        String str="YoU ARe SO SmArT";
+
+        String lstring= str.toLowerCase();
+        System.out.println(lstring);
+    }
+}
