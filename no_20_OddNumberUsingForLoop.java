df08b0c17900795d7daa3820a938bb94ef62dc3c
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-06-12 23:03:01 +0530

    Java Program

---
+++ no_20_OddNumberUsingForLoop.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class no_20_OddNumberUsingForLoop {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println(2*i+1);
+        }
+    }
+}
