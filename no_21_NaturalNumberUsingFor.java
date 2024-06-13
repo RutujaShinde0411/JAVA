13f12b71244ab6cc6e20be4fcda3666130bd545b
RutujaShinde0411 <rutujajshinde46@gmail.com>
2024-06-13 22:51:48 +0530

    master

---
+++ no_21_NaturalNumberUsingFor.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class no_21_NaturalNumberUsingFor {
+    public static void main(String[] args) {
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
